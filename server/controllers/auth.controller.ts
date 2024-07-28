536059b2e1f8ec3fbdfbea94a92ab950be48bae3
Mahmud Lukman <mistamann49@gmail.com>
2024-07-28 12:26:49 +0100

    update access token added

--- server/controllers/auth.controller.ts
+++ server/controllers/auth.controller.ts
@@ -181,3 +181,50 @@ export const logoutUser = catchAsyncError(
     }
   }
 );
+
+// update access token
+export const updateAccessToken = catchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const refresh_token = req.cookies.refresh_token as string;
+      const decoded = jwt.verify(
+        refresh_token,
+        process.env.REFRESH_TOKEN as string
+      ) as JwtPayload;
+
+      const message = 'Could not refresh token';
+      if (!decoded) {
+        return next(new ErrorHandler(message, 400));
+      }
+      const session = await redis.get(decoded.id as string);
+
+      if (!session) {
+        return next(new ErrorHandler("Please login to access this resource", 400));
+      }
+      const user = JSON.parse(session);
+
+      const accessToken = jwt.sign(
+        { id: user._id },
+        process.env.ACCESS_TOKEN as string,
+        { expiresIn: '5m' }
+      );
+
+      const refreshToken = jwt.sign(
+        { id: user._id },
+        process.env.REFRESH_TOKEN as string,
+        { expiresIn: '3d' }
+      );
+
+      req.user = user;
+
+      res.cookie('access_token', accessToken, accessTokenOptions);
+      res.cookie('refresh_token', refreshToken, refreshTokenOptions);
+
+      await redis.set(user._id, JSON.stringify(user), "EX", 604800) // 7 days
+
+      next();
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 400));
+    }
+  }
+);
