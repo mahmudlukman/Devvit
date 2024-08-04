8fd08e1b345f945aeba7bf2f00f0d3ad22ac20ee
Mahmud Lukman <mistamann49@gmail.com>
2024-08-04 11:12:53 +0100

    social auth completed

--- client/components/auth/Social.tsx
+++ client/components/auth/Social.tsx
@@ -8,7 +8,7 @@ import { useSearchParams } from 'next/navigation';
 
 export const Social = () => {
   const searchParams = useSearchParams()
-  const callbackUrl = searchParams.get("callbackUrl")
+  const callbackUrl = searchParams?.get("callbackUrl")
   const onClick = (provider: 'google' | 'github') => {
     signIn(provider, {
       callbackUrl: callbackUrl || DEFAULT_LOGIN_REDIRECT,
