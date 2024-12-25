ff3e34c427a8b15656a501bc955573ec101059d4
mahmud lukman <mistamann49@gmail.com>
2024-12-25 11:16:17 +0100

    login page bug fix

--- client/app/(auth)/login/page.tsx
+++ client/app/(auth)/login/page.tsx
@@ -2,11 +2,15 @@ import { Suspense } from "react";
 import { LoginForm } from "../../../components/auth/LoginForm";
 
 const Login = () => {
+  return <LoginForm />;
+};
+
+const Page = () => {
   return (
     <Suspense fallback={<div>Loading...</div>}>
-      <LoginForm />
+      <Login />
     </Suspense>
   );
 };
 
-export default Login;
+export default Page;
